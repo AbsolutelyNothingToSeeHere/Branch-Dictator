6f33e4c03822b93511d9a847adf10aea22763b84
Spencer <SpencerKaiser@users.noreply.github.com>
2020-06-06 22:28:23 -0500

    Initial implementation of the branch protection functionality (#12)
    * Initial implementation of the branch protection functionality
    
    * Initial implementation of the branch protection functionality
    
    * Adding delay to prevent 404 with GitHub api

--- src/api/webhook/repository.ts
+++ src/api/webhook/repository.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import crypto from 'crypto';
 import logger from '../../logger';
+import { RepositoryWebhookPayload } from '../../types/github';
+import github from '../../utils/github';
+import getRequiredEnvVar from '../../utils/getRequiredEnvVar';
 
-const webhookSecret = process.env.WEBHOOK_SECRET;
-if (!webhookSecret && process.env.NODE_ENV !== 'test') {
-  logger.error('Missing required environment variable: WEBHOOK_SECRET');
-  process.exit(1);
-}
+const webhookSecret = getRequiredEnvVar('WEBHOOK_SECRET');
 
 const repository = express.Router();
-repository.post('/repository', (req, res) => {
+repository.post('/repository', async (req, res) => {
   const { body, headers } = req;
   const githubSignature = headers['x-hub-signature'];
   const hash = crypto.createHmac('sha1', webhookSecret).update(JSON.stringify(body)).digest('hex');
@@ -18,6 +17,28 @@ repository.post('/repository', (req, res) => {
     // Unauthorized request
     res.sendStatus(401);
     return;
+  }
+
+  const event = body as RepositoryWebhookPayload;
+  if (event.action === 'created') {
+    try {
+      // Give GitHub a 500ms a create the first commmit/branch
+      // Without this, we'll make the request too quickly and the request will fail with  404
+      await new Promise((resolve) => setTimeout(resolve, 500));
+      await github.repos.updateBranchProtection({
+        branch: event.repository.default_branch,
+        repo: event.repository.name,
+        owner: event.repository.owner.login,
+        enforce_admins: true,
+        required_pull_request_reviews: null,
+        required_status_checks: null,
+        restrictions: null,
+      });
+    } catch (error) {
+      logger.error(`Something went wrong updating branch protection: ${error.message} - `, error);
+      res.sendStatus(500);
+      return;
+    }
   }
 
   res.sendStatus(200);
---
+++ src/types/github.ts
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+export interface RepositoryWebhookPayload {
+  action: 'created' | string;
+  repository: {
+    name: string;
+    owner: {
+      login: string;
+    };
+    default_branch: string;
+  };
+  // organization
+  // installation
+  // sender
+}
